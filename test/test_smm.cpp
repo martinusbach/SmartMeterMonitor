2f2d6d34b541fe8098fe02d6106cfb95be73629f
Developer Martien <dev@oppeneer.net>
2020-01-18 22:27:37 +0100

    First attempt for parsing the individual telegram entries.

--- test/test_smm.cpp
+++ test/test_smm.cpp
@@ -6,6 +6,8 @@
 #include <catch2/catch.hpp>
 
 #include <iostream>
+#include <map>
+#include <memory>
 #include <regex>
 
 
@@ -23,12 +25,107 @@ struct sregex_range : std::pair<std::sregex_iterator, std::sregex_iterator>
 };
 
 
+class ObisEntry
+{
+public:
+    ObisEntry() = default;
+    virtual ~ObisEntry() = default;
+    virtual void parse(const std::string& s) = 0;
+};
+
+class IntObisEntry : public ObisEntry
+{
+public:
+    IntObisEntry()
+        : ObisEntry()
+        , value_()
+    { }
+
+    void parse(const std::string& s)
+    {
+        value_ = std::stoi(s);
+        std::cout << "parsed int\n";
+    }
+private:
+    int value_;
+};
+
+class TimestampObisEntry : public ObisEntry
+{
+public:
+    TimestampObisEntry()
+        : year_()
+        , month_()
+        , day_()
+    { }
+    void parse(const std::string& s)
+    {
+        const std::regex re("(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[012])([0-9]{2})(2[0-3]|[0-1][0-9])([0-5][0-9])([0-5][0-9])([SW])");
+        std::smatch match;
+        if (!std::regex_search(s, match, re)) {
+            throw std::runtime_error("date not found!");
+        }
+        year_ = std::stoi(match.str(1));
+        month_ = std::stoi(match.str(2));
+        day_ = std::stoi(match.str(3));
+        hour_ = std::stoi(match.str(4));
+        min_ = std::stoi(match.str(5));
+        sec_ = std::stoi(match.str(6));
+        dst_ = match.str(7)[0];
+
+        std::cout << "parsed date " << dst_ << "\n";
+    }
+private:
+    int year_;
+    int month_;
+    int day_;
+    int hour_;
+    int min_;
+    int sec_;
+    char dst_;
+};
+
+class MeasurementObisEntry : public ObisEntry
+{
+public:
+    MeasurementObisEntry()
+        : ObisEntry()
+        , value_()
+    { }
+
+    void parse(const std::string& s)
+    {
+        const std::regex re(R"((\d+)(?:\.(\d+))?\*(\w+))");
+        std::smatch match;
+        if (!std::regex_search(s, match, re)) {
+            throw std::runtime_error("date not found!");
+        }
+        value_ = std::stod(match.str(1));
+        if (match.str(2) != "") {
+            value_ += std::stod("0." + match.str(2));
+        }
+        unit_ = match.str(3);
+
+        std::cout << "parsed a measurent: " << value_ << unit_ << ".\n";
+    }
+private:
+    double value_;
+    std::string unit_;
+};
+
+
 TEST_CASE("First parser test", "[parser]")
 {
     REQUIRE(true);
 
     FileTelegramGenerator gen;
     std::string telegram = gen.new_telegram();
+
+
+    std::map<std::string, std::unique_ptr<ObisEntry>> telegram_entries;
+    telegram_entries.emplace("1-3:0.2.8", std::make_unique<IntObisEntry>());
+    telegram_entries.emplace("0-0:1.0.0", std::make_unique<TimestampObisEntry>());
+    telegram_entries.emplace("1-0:1.8.1", std::make_unique<MeasurementObisEntry>());
 
 
     const std::regex re_header(R"(/([A-Z]{3}\d[A-Z0-9]{16}))");
@@ -38,6 +135,7 @@ TEST_CASE("First parser test", "[parser]")
     }
 
     const std::regex re_OBISID_and_data(R"(([01]-[03]:\d{1,2}\.\d{1,2}\.\d{1,2})\(([^()]*)\)(.*))");
+
     for (const auto& match : sregex_range(telegram.begin(), telegram.end(), re_OBISID_and_data)) {
         const std::string& obis_id = match.str(1);
         const std::string& data = match.str(2);
@@ -47,7 +145,14 @@ TEST_CASE("First parser test", "[parser]")
             std::cout << ", extra data: `" << data_extra << "`";
         }
         std::cout << "\n";
+
+        // start parsing
+        if (auto& tg_entry = telegram_entries[obis_id]) {
+            tg_entry ->parse(data);
+        }
+        else {
+            std::cout << "no parse\n";
+        }
     }
 
 }
-
